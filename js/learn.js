15a4c1ce0b9944f0ea71c110e5ea287d987ba736
Carl Cortright <ckcortright@gmail.com>
2016-11-27 16:24:40 -0700

    Added state vector and reward function

--- js/learn.js
+++ js/learn.js
@@ -4,11 +4,11 @@ var player = new Player();
 
 // create an environment object
 var env = {};
-env.getNumStates = function() { return 8; }
-env.getMaxNumActions = function() { return 4; }
+env.getNumStates = function() { return 10; }
+env.getMaxNumActions = function() { return 3; }
 
 // create the DQN agent
-var spec = { alpha: 0.01 } // see full options on DQN page
+var spec = { alpha: 0.01, experience_size: 5000} // see full options on DQN page
 agent = new RL.DQNAgent(env, spec);
 
 setInterval(function(){ // start the learning loop
@@ -17,20 +17,63 @@ setInterval(function(){ // start the learning loop
     game.playIntro();
     game.play();
   } else if (game.activated) {
+    // We survived the last action, set the reward to +1
+    reward = 1;
 
-    player.do(Player.actions.JUMP);
+    // Create the feature vector
+    s = [0,0,0,0,0,0,0,0,0,0];
+    if(game.horizon.obstacles.length == 0){
+      s[0] = game.currentSpeed;
+    } else if (game.horizon.obstacles.length == 1){
+      s[0] = game.currentSpeed;
+      s[1] = game.horizon.obstacles[0].xPos - game.horizon.obstacles[0].spritePos.x;
+      s[2] = game.horizon.obstacles[0].yPos;
+      s[3] = game.horizon.obstacles[0].width;
+    } else if (game.horizon.obstacles.length == 2){
+      s[0] = game.currentSpeed;
+      s[1] = game.horizon.obstacles[0].xPos - game.horizon.obstacles[0].spritePos.x;
+      s[2] = game.horizon.obstacles[0].yPos;
+      s[3] = game.horizon.obstacles[0].width;
+      s[4] = game.horizon.obstacles[1].xPos - game.horizon.obstacles[1].spritePos.x;
+      s[5] = game.horizon.obstacles[1].yPos;
+      s[6] = game.horizon.obstacles[1].width;
+    } else if (game.horizon.obstacles.length >= 3){
+      s[0] = game.currentSpeed;
+      s[1] = game.horizon.obstacles[0].xPos - game.horizon.obstacles[0].spritePos.x;
+      s[2] = game.horizon.obstacles[0].yPos;
+      s[3] = game.horizon.obstacles[0].width;
+      s[4] = game.horizon.obstacles[1].xPos - game.horizon.obstacles[1].spritePos.x;
+      s[5] = game.horizon.obstacles[1].yPos;
+      s[6] = game.horizon.obstacles[1].width;
+      s[7] = game.horizon.obstacles[2].xPos - game.horizon.obstacles[2].spritePos.x;
+      s[8] = game.horizon.obstacles[2].yPos;
+      s[9] = game.horizon.obstacles[2].width;
+    }
 
-    console.log(game.horizon.obstacles);
+    var action = agent.act(s); // s is an array of length 10
 
+    // Take the action chosen by the agent
+    if(action == 0){
+      player.do(Player.actions.IDLE);
+    } else if(action == 1){
+      player.do(Player.actions.JUMP);
+    } else if (action == 2) {
+      player.do(Player.actions.DUCK);
+    }
+    console.log(reward);
+    agent.learn(reward);
 
   } else {
+    reward = -100;
+    console.log(reward);
+    agent.learn(reward);
     game.restart();
   }
 
-  // var action = agent.act(s); // s is an array of length 8
+
   // //... execute action in environment and get the reward
-  // agent.learn(reward); // the agent improves its Q,policy,model, etc. reward is a float
+  //  // the agent improves its Q,policy,model, etc. reward is a float
 
 
 
-}, 500);
+}, 50);
