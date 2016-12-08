98cf2fe10d34438481af985c459eaeada56aebf4
Carl Cortright <ckcortright@gmail.com>
2016-12-07 18:54:38 -0700

    Updated readme with running instructions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # DinoRL
+
 Reinforcement learning for the Google Chrome offline dinosaur game 
+
+### Running
+
+Clone from github, making sure you use the --recursive flag
+
+``` git clone https://github.com/DinoRL/Dino-game.git --recursive ```
+
+Run using google chrome browser.
+
+ctrl-o -> Navigate to directory with code, and open index.html
--- js/chart.js
+++ js/chart.js
@@ -8,7 +8,12 @@ google.charts.setOnLoadCallback(function () {
     data.addColumn('number', 'Successful Jumps');
     data.addRows([[0, 0]]);
     chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-    chart.draw(data)
+    var options = {
+          title: 'Successful Jumps',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+    chart.draw(data, options)
   });
 
 function updateChart(row){
